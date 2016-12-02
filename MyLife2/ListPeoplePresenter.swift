00013ac094c610afa3cce4df840c64e7099ac602
Ivan Rublev <ivan@ivanrublev.me>
2016-12-02 15:48:28 +0300

    10.2 presentList is released.

--- MyLife2/ListPeoplePresenter.swift
+++ MyLife2/ListPeoplePresenter.swift
@@ -10,16 +10,32 @@ import UIKit
 
 // MARK: Boundary protocol
 protocol ListPeoplePresenterOutput: class {
-    
+    func displayList(_ response: ListPeople.Response.ViewModel)
 }
 
 // MARK: Class
 /**
-    Class to format the data response from the Interactor and pass the result view model to the View Controller
+ Class to format the data response from the Interactor and pass the result view model to the View Controller
  */
 class ListPeoplePresenter: ListPeopleInteractorOutput {
     weak var output: ListPeoplePresenterOutput?
     
     // MARK: Presentation logic
+    func presentList(_ response: ListPeople.Response) {
+        var itemsViewModel: [ListPeople.Response.ViewModel.ItemViewModel] = []
+        
+        response.items.forEach { (item) in
+            let name = item.name?.uppercased()
+            
+            let itemViewModel = ListPeople.Response.ViewModel
+                                .ItemViewModel(identifier: item.identifier,
+                                               preparedName: name,
+                                               preparedImage: item.image)
+            
+            itemsViewModel.append(itemViewModel)
+        }
 
+        let viewModel = ListPeople.Response.ViewModel(itemsViewModel: itemsViewModel)
+        output?.displayList(viewModel)
+    }
 }
